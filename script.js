6970c13170ed2d61549e5c8e64150d5691eecb3a
Christopher Marie-Angelique <christopher2211.angelique@gmail.com>
2022-05-14 20:14:53 +0200

    Ajout des fichiers

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+AOS.init();
+
 window.addEventListener("scroll", function(){
     var nav = this.document.querySelector("nav");
+    const btnScroll = document.querySelector("#btn-scroll");
+
     nav.classList.toggle("sticky", window.scrollY > 20 );
-}) 
+    btnScroll.classList.toggle("active", window.scrollY > 600);
+    
+});
+
+const btnScroll = document.querySelector("#btn-scroll");
+    btnScroll.addEventListener("click", function(){
+    window.scrollTo(0, 0);
+    console.log("click!");
+});
