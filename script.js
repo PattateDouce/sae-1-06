e60ffa4a339dc57d8f9cec06c6956591d62a2aae
Christopher Marie-Angelique <christopher2211.angelique@gmail.com>
2022-05-18 11:55:34 +0200

    Ajout des fichiers

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-AOS.init();
+// Librairie pour les animations
+AOS.init({
 
+    once: true
+
+});
+
+// Menu responsive
+let toggle = document.querySelector(".toggle");
+let body = document.querySelector("body");
+
+toggle.addEventListener('click', function(){
+    body.classList.toggle('open');
+});
+///////////////////////////////////
+
+// background du nav au scroll
 window.addEventListener("scroll", function(){
-    var nav = this.document.querySelector("nav");
+    var header = this.document.querySelector("header");
     const btnScroll = document.querySelector("#btn-scroll");
 
-    nav.classList.toggle("sticky", window.scrollY > 20 );
+    header.classList.toggle("sticky", window.scrollY > 20 );
     btnScroll.classList.toggle("active", window.scrollY > 600);
     
 });
+///////////////////////////////////
 
+// Bouton de scroll
 const btnScroll = document.querySelector("#btn-scroll");
     btnScroll.addEventListener("click", function(){
     window.scrollTo(0, 0);
-    console.log("click!");
 });
